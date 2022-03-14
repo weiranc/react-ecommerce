082b581584b6946fe38a4d58e6d068f155f104a0
Weiran (Rebecca) Cheng <91348196+weiranc@users.noreply.github.com>
2022-03-14 12:39:45 -0700

    obtain dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios.get('https://fakestoreapi.com/products')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(err => console.error(err))
+  }
+
   return (
     <div className='App'>
       HELLO
