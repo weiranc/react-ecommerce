1f99da3273a4d1394faef6611c675ff25ea37ba9
Weiran (Rebecca) Cheng <91348196+weiranc@users.noreply.github.com>
2022-03-15 22:16:40 -0700

    utilize express cors middleware to relax the security applied to an API

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3001;
+const cors = require('cors');
 const router = require('./router.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
