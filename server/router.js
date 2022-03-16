fb51d484a33d9ad083ca4bc2dd8ed222d18474c2
Weiran (Rebecca) Cheng <91348196+weiranc@users.noreply.github.com>
2022-03-15 23:41:06 -0700

    add route for all the methods

--- server/router.js
+++ server/router.js
@@ -2,5 +2,11 @@ const router = require('express').Router();
 const controller = require('./controller.js');
 
 router.route('/products').get(controller.getAll);
+router.route('/carts').get(controller.getCart);
+router
+  .route('/carts/:id')
+  .post(controller.addToCart)
+  .put(controller.updateCart)
+  .delete(controller.removeFromCart);
 
 module.exports = router;
