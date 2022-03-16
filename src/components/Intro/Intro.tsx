73d1d96e97233ffac9e89ef6c07b1c1cefb0f0c3
Weiran (Rebecca) Cheng <91348196+weiranc@users.noreply.github.com>
2022-03-16 14:13:23 -0700

    implement react mui carousel to show a list of product images

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -4,9 +4,9 @@ import Carousel from 'react-material-ui-carousel';
 const Intro = () => {
 
   return (
-    <Carousel>
-      <img className='intro_carousel' src={require('../img/clothes.jpg')} alt='clothes' />
-      <img className='intro_carousel' src={require('../img/items.jpg')} alt='items' />
+    <Carousel className='intro_carousel'>
+      <img className='carousel_img' src={require('../img/clothes.jpg')} alt='clothes' />
+      <img className='carousel_img' src={require('../img/items.jpg')} alt='items' />
     </Carousel>
   );
 };
