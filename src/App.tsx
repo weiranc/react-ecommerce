e1b5bb0445c1463e8b342515ca5d4edd3cb963a3
Weiran (Rebecca) Cheng <91348196+weiranc@users.noreply.github.com>
2022-03-16 18:59:15 -0700

    add router to render different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ProductList from './components/products/ProductList';
 import { Grid, Drawer } from '@mui/material';
@@ -7,6 +8,7 @@ import LeftBar from './components/LeftBar/LeftBar';
 import SearchBar from './components/SearchBar/SearchBar';
 import Cart from './components/Cart/Cart';
 import Intro from './components/Intro/Intro';
+import Checkout from './components/Checkout/Checkout';
 
 export type ProductType = {
   id: number;
@@ -19,7 +21,7 @@ export type ProductType = {
   rating: Record<string, number>;
 };
 
-const App = () => {
+const Home = () => {
   const [data, setData] = useState([] as ProductType[]);
   const [cartOpen, setCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([] as ProductType[]);
@@ -70,7 +72,9 @@ const App = () => {
         .put(`${process.env.REACT_APP_SERVER}/carts/${selectedProduct.id}`, {
           newQuantity: newCart[0],
         })
-        .then((response) => {console.log('Updated')})
+        .then((response) => {
+          console.log('Updated');
+        })
         .catch((err) => console.error(err));
     } else {
       let data = { ...selectedProduct, amount: 1 };
@@ -90,7 +94,7 @@ const App = () => {
     );
     if (isProductAmountLargerThanOne) {
       const newCart = cartItems.filter((product) => {
-        return product.id === selectedProduct.id
+        return product.id === selectedProduct.id;
       });
       newCart[0].amount--;
       axios
@@ -100,8 +104,9 @@ const App = () => {
         .then((response) => console.log('Updated'))
         .catch((err) => console.error(err));
     } else {
-      axios
-        .delete(`${process.env.REACT_APP_SERVER}/carts/${selectedProduct.id}`)
+      axios.delete(
+        `${process.env.REACT_APP_SERVER}/carts/${selectedProduct.id}`
+      );
     }
     setTotalItems(totalItems - 1);
   };
@@ -130,6 +135,14 @@ const App = () => {
     }
   };
 
+const getTotalPrice = () => {
+    const totalPrice = cartItems.reduce(
+      (total, item) => item.price * item.amount + total,
+      0
+    );
+    return totalPrice.toFixed(2);
+  };
+
   return (
     <div className='App'>
       <SearchBar
@@ -137,15 +150,24 @@ const App = () => {
         openCart={openCart}
         setSearch={setSearch}
       />
-
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-        <Cart
-          cartItems={cartItems}
-          handleAddToCart={handleAddToCart}
-          handleRemoveFromCart={handleRemoveFromCart}
-        />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <Cart
+                  cartItems={cartItems}
+                  handleAddToCart={handleAddToCart}
+                  handleRemoveFromCart={handleRemoveFromCart}
+                  getTotalPrice={getTotalPrice}
+                />
+              }
+            />
+            <Route path='/payment' element={<Checkout getTotalPrice={getTotalPrice}/>} />
+          </Routes>
+        </BrowserRouter>
       </Drawer>
-
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <Intro />
@@ -164,4 +186,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
